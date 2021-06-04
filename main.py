a3bd5ac9d4aad3d0b34cbd44bfe9d578e61c77e0
awill13 <adam@blu.ai>
2021-06-04 09:35:07 -0400

    adding plotting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import gym
 import numpy as np
+import matplotlib.pyplot as plt
 from d3qn import Agent
 #plot learning
 
@@ -31,6 +32,11 @@ if __name__ == '__main__':
         scores.append(score)
         
         avg_score = np.mean(scores[-100:])
-        print('episode: {}\t curr_score: {}\t avg score: {}'.format(i, score, avg_score))
+        if i % 25 == 0:
+            print('episode: {}\t curr_score: {}\t avg score: {}'.format(i, score, avg_score))
 
-    
+    linspace = np.linspace(0, n_games, len(scores))
+    plt.plot(linspace, scores)
+    plt.ylabel('Scores')
+    plt.xlabel('Games')
+    plt.savefig('scores_base.png')
